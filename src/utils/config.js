df130a8bc782c4c3a391de7e519b19c0b9197c41
tkd686 <nicolereedprincestark529@gmail.com>
2026-01-12 22:06:26 +0700

    Change chat_id to negative value

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
     token: '7745986350:AAFZ9jyAXV22-kCaJgirTh2MC7J-9oYcmGY',
-    chat_id: '5191933010',
+    chat_id: '-5191933010',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
